851c2c101bc8491e8a6867881a01bcc983c7001c
mark3lau <mark3lau@gmail.com>
2022-07-12 10:45:41 +0000

    Cache the dom

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,22 +13,15 @@ document.addEventListener("DOMContentLoaded", function() {
     }
 })
 
-
-
-function runGame() {
-
-}
-
-function checkAnswer() {
-
-}
-
-function incrementPlayerScore() {
-
-}
-
-function incrementComputerScore() {
-
-}
-
+const playerScore = 0;
+const computerScore = 0;
+const playerScore_span = document.getElementById("playerScore");
+const computerScore_span = document.getElementById("computerScore");
+const scoreBoard_div = document.querySelector(".scoreBoard");
+const result_div = document.querySelector(".result");
+const rock_div = document.getElementById("rock");
+const paper_div = document.getElementById("paper");
+const scissors_div = document.getElementById("scissors");
+const lizard_div = document.getElementById("lizard");
+const spock_div = document.getElementById("spock");
 
